36a3a0d53b71c5b43504c6bbb2ce2f5a4f068fb4
William Chu <william@williamchu.org>
2019-10-21 22:13:16 -0500

    Stop screensaver on coin insert

--- src/ScreenSaverModule.cpp
+++ src/ScreenSaverModule.cpp
@@ -6,13 +6,14 @@
 
 void ScreenSaverModule::init()
 {
+	core()->eventBroker().addObserver(this);
+	subscribe<CoinInsertEvent>();
 }
 void ScreenSaverModule::shutdown()
 {
 }
 void ScreenSaverModule::update(float elapsed)
 {
-	auto videoControl = core()->module<VideoControlModule>();
 	const float screenSaverTime = Settings::get<float>("screenSaverTime");
 
 	Cinnabar::Vector2i mousePosition;
@@ -21,24 +22,46 @@ void ScreenSaverModule::update(float elapsed)
 	if(mousePosition != _lastMousePosition)
 	{
 		_lastMousePosition = mousePosition;
-		_timer = 0.0f;
-		if(_active && videoControl->isPlayerOpen())
-		{
-			core()->eventBroker().emit(VideoStopRequestEvent());
-			core()->eventBroker().emit(DepartmentDeselectEvent());
-			_active = false;
-		}
+		_wake();
 	}
 	else
 	{
 		_timer += elapsed;
-		if(_timer >= screenSaverTime && !videoControl->isPlayerOpen())
-		{
-			core()->eventBroker().emit(VideoPlayRequestEvent{
-				"resources/video/promo.mp4",
-				true
-			});
-			_active = true;
-		}
+		if(_timer >= screenSaverTime)
+			_sleep();
+	}
+}
+void ScreenSaverModule::onEvent(const Cinnabar::EventBroker::Event* event)
+{
+	if(event->is<CoinInsertEvent>())
+	{
+		onEvent(event->as<CoinInsertEvent>());
+	}
+}
+void ScreenSaverModule::onEvent(const CoinInsertEvent& event)
+{
+	_wake();
+}
+void ScreenSaverModule::_sleep()
+{
+	auto videoControl = core()->module<VideoControlModule>();
+	if(!videoControl->isPlayerOpen())
+	{
+		core()->eventBroker().emit(VideoPlayRequestEvent{
+			"resources/video/promo.mp4",
+			true
+		});
+		_active = true;
+	}
+}
+void ScreenSaverModule::_wake()
+{
+	_timer = 0.0f;
+	auto videoControl = core()->module<VideoControlModule>();
+	if(_active && videoControl->isPlayerOpen())
+	{
+		core()->eventBroker().emit(VideoStopRequestEvent());
+		core()->eventBroker().emit(DepartmentDeselectEvent());
+		_active = false;
 	}
 }
--- src/ScreenSaverModule.h
+++ src/ScreenSaverModule.h
@@ -1,19 +1,28 @@
 #pragma once
+#include "ASerial.h"
 #include "Cinnabar/EventBroker.h"
 #include "Cinnabar/Math/Vector2.h"
 #include "Cinnabar/Module.h"
 #include "Enumerations.h"
-#include "ASerial.h"
+#include "Events.h"
 
-class ScreenSaverModule : public Cinnabar::Module<ScreenSaverModule>
+class ScreenSaverModule
+	: public Cinnabar::Module<ScreenSaverModule>,
+	Cinnabar::EventBroker::Observer
 {
 public:
 	void init() override;
 	void shutdown() override;
 	void update(float) override;
 
+	void onEvent(const Cinnabar::EventBroker::Event*) override;
+	void onEvent(const CoinInsertEvent&);
+
 private:
 	float _timer = 0.0f;
 	bool _active = false;
 	Cinnabar::Vector2i _lastMousePosition;
+
+	void _sleep();
+	void _wake();
 };
